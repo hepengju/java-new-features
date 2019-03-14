07d9bf00e01f277adce350dd14cc4977c6826cdb
dalianpai <wang.gr@topcheer.com>
2019-03-14 21:15:37 +0800

    Java10 new features add by wgr

---
+++ src/main/java/com/hepengju/java10/new03_localhandshake/_LocalHandShake.java
@@ -0,0 +1,20 @@
+package com.hepengju.java10.new03_localhandshake;
+
+/**
+ * 线程本地握手
+ * 
+ * <pre>
+ *  JEP 312: Thread-Local Handshakes
+ *  说明：
+ *        * JDK 10将引入一种在线程上执行回调的新方法
+ *        * 因此这将会很方便能停止单个线程而不是停止全部线程或者一个都不停。
+ *  
+ *  @see <a href="http://openjdk.java.net/jeps/312">
+ *  
+ * <pre>
+ * 
+ * @author WGR
+ */
+public class _LocalHandShake {
+
+}
