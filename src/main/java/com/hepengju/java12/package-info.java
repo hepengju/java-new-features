387f353a876beb93c9e3a9e2f65e423792a00435
何亦冷 <he_pengju@163.com>
2019-04-07 15:29:23 +0800

    Java12新特性引入

---
+++ src/main/java/com/hepengju/java12/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * Java12的新特性
+ *
+ * <ol>
+ * <li>
+ * <li>
+ * <li>
+ * <li> 其他杂项改进
+ *      <ul>
+ *      <li>
+ *      <li>
+ *      </ul>
+ * </ol>
+ *
+ * @see <a href="https://www.oracle.com/technetwork/java/javase/12-relnote-issues-5211422.html#NewFeature">What's New in JDK 12</a>
+ *
+ * @author hepengju
+ *
+ */
+package com.hepengju.java12;
