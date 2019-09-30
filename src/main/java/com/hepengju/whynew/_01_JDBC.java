6994e77b24104538dbd1f679fdcf9a788cd7c5ce
何鹏举 <he_pengju@163.com>
2019-10-01 01:09:55 +0800

    修改jdbc连接参数为云主机的连接参数, 添加创建表和插入数据的脚本

--- src/main/java/com/hepengju/whynew/_01_JDBC.java
+++ src/main/java/com/hepengju/whynew/_01_JDBC.java
@@ -18,9 +18,12 @@ public class _01_JDBC {
 
     // JDBC的四大连接参数
     private String className = "com.mysql.jdbc.Driver";
-    private String url       = "jdbc:mysql://192.168.180.100:3306/oss6?useSSL=false";
-    private String username  = "root";
-    private String password  = "root";
+    private String jdbcUrl   = "jdbc:mysql://mysql57.hepengju.cn:3306/java-new-features?useSSL=false";
+    private String username  = "java123";
+    private String password  = "java123";
+
+    // 测试的查询语句
+    private String TEST_SQL = "select * from person";
 
     /**
      * 简单测试
@@ -34,8 +37,8 @@ public class _01_JDBC {
      */
     @Test public void testJDBC_00() throws ClassNotFoundException, SQLException {
         Class.forName(className);
-        Connection conn = DriverManager.getConnection(url, username, password);
-        PreparedStatement pstmt = conn.prepareStatement("select * from z010_role");
+        Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+        PreparedStatement pstmt = conn.prepareStatement(TEST_SQL);
         ResultSet rst = pstmt.executeQuery();
         int columnCount = rst.getMetaData().getColumnCount();
         while (rst.next()) {
@@ -62,8 +65,8 @@ public class _01_JDBC {
         ResultSet rst = null;
         
         try {
-            conn = DriverManager.getConnection(url, username, password);
-            pstmt = conn.prepareStatement("select * from z010_role");
+            conn = DriverManager.getConnection(jdbcUrl, username, password);
+            pstmt = conn.prepareStatement(TEST_SQL);
             rst = pstmt.executeQuery();
             int columnCount = rst.getMetaData().getColumnCount();
             while (rst.next()) {
@@ -117,8 +120,8 @@ public class _01_JDBC {
         ResultSet rst = null;
         
         try {
-            conn = DriverManager.getConnection(url, username, password);
-            pstmt = conn.prepareStatement("select * from z010_role");
+            conn = DriverManager.getConnection(jdbcUrl, username, password);
+            pstmt = conn.prepareStatement(TEST_SQL);
             rst = pstmt.executeQuery();
             int columnCount = rst.getMetaData().getColumnCount();
             while (rst.next()) {
@@ -163,8 +166,8 @@ public class _01_JDBC {
      * 实现AutoCloseable接口的可以放在try后面的小括号里面自动关闭
      */
     @Test public void testJDBC_03() {
-        try (Connection conn = DriverManager.getConnection(url, username, password);
-             PreparedStatement pstmt = conn.prepareStatement("select * from z010_role");
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+             PreparedStatement pstmt = conn.prepareStatement(TEST_SQL);
              ResultSet rst = pstmt.executeQuery()) {
             int columnCount = rst.getMetaData().getColumnCount();
             while (rst.next()) {
@@ -185,40 +188,40 @@ public class _01_JDBC {
      * <br> 已经有资源了, 需要在内部关闭
      * 
      */
-    @Test public void testJDBC_04(Connection conn,PreparedStatement pstmt,ResultSet rst){
+    @Test public void testJDBC_04(){
         // 对于JDBC中的这种写法不安全,在某些情况下有用
-        //try {
-        //    Connection conn = DriverManager.getConnection(url, username, password);
-        //    PreparedStatement pstmt = conn.prepareStatement("select * from z010_role");
-        //    ResultSet rst = pstmt.executeQuery();
-        //    try (conn; pstmt; rst) {
-        //        int columnCount = rst.getMetaData().getColumnCount();
-        //        while (rst.next()) {
-        //            for (int i = 1; i <= columnCount; i++) {
-        //                Object obj = rst.getObject(i);
-        //                System.out.print(obj + "\t");
-        //            }
-        //            System.out.println();
-        //        }
-        //    } catch (SQLException e) {
-        //        e.printStackTrace();
-        //    }
-        //} catch (SQLException e) {
-        //    e.printStackTrace();
-        //}
-        
-        try (conn; pstmt; rst) {
-            int columnCount = rst.getMetaData().getColumnCount();
-            while (rst.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    Object obj = rst.getObject(i);
-                    System.out.print(obj + "\t");
+        try {
+            Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+            PreparedStatement pstmt = conn.prepareStatement(TEST_SQL);
+            ResultSet rst = pstmt.executeQuery();
+            try (conn; pstmt; rst) {
+                int columnCount = rst.getMetaData().getColumnCount();
+                while (rst.next()) {
+                    for (int i = 1; i <= columnCount; i++) {
+                        Object obj = rst.getObject(i);
+                        System.out.print(obj + "\t");
+                    }
+                    System.out.println();
                 }
-                System.out.println();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        
+        //try (conn; pstmt; rst) {
+        //    int columnCount = rst.getMetaData().getColumnCount();
+        //    while (rst.next()) {
+        //        for (int i = 1; i <= columnCount; i++) {
+        //            Object obj = rst.getObject(i);
+        //            System.out.print(obj + "\t");
+        //        }
+        //        System.out.println();
+        //    }
+        //} catch (SQLException e) {
+        //    e.printStackTrace();
+        //}
         
     }
     
@@ -226,8 +229,8 @@ public class _01_JDBC {
      * Java10中的var变量
      */
     @Test public void testJDBC_05() {
-        try (var conn = DriverManager.getConnection(url, username, password);
-             var pstmt = conn.prepareStatement("select * from z010_role");
+        try (var conn = DriverManager.getConnection(jdbcUrl, username, password);
+             var pstmt = conn.prepareStatement(TEST_SQL);
              var rst = pstmt.executeQuery()) {
             int columnCount = rst.getMetaData().getColumnCount();
             while (rst.next()) {
