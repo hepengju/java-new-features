4a9c44ee51b62516b27dcf5ca9d5b23fc0fde60f
何鹏举 <hepengju@fofund.com.cn>
2024-07-17 16:12:46 +0800

    java20

--- src/com/hepengju/java20/package-info.java
+++ src/com/hepengju/java20/package-info.java
@@ -1,1 +1,19 @@
+/**
+ * Java20的新特性
+ *
+ * <pre>
+ *      429: Scoped Values (Incubator)                          范围值（孵化）
+ *      432: Record Patterns (Second Preview)                   记录模式（第二次预览）
+ *      433: Pattern Matching for switch (Fourth Preview)       switch模式匹配（第四次预览）
+ *      434: Foreign Function & Memory API (Second Preview)     外部函数和内存API（第二次预览）
+ *      436: Virtual Threads (Second Preview)                   虚拟线程（第二次预览）
+ *      437: Structured Concurrency (Second Incubator)          结构化并发（第二次孵化）
+ *      438: Vector API (Fifth Incubator)                       矢量API（第五次孵化）
+ * </pre>
+ *
+ * @see <a href="http://openjdk.java.net/projects/jdk/20/">OpenJDK20</a>
+ * @see <a href="https://www.oracle.com/java/technologies/javase/20-relnote-issues.html">OracleJDK20</a>
+ *
+ * @author hepengju
+ */
 package com.hepengju.java20;
